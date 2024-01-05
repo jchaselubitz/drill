0f5be58e95983013b808ce2378563c3ff0ffbc9a
jchaselubitz <jchaselubitz@gmail.com>
2024-01-05 14:04:09 +0100

    make sidebar work for mobile

---
+++ src/lib/navigation/SideBarItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let text = '';
+	export let path = '';
+</script>
+
+<div
+	class="my-2 bg-gray-200 hover:bg-gray-400 p-2 w-full text-gray-800 text-xs font-semibold uppercase text-center rounded-lg"
+>
+	<a href={path}>{text}</a>
+</div>
